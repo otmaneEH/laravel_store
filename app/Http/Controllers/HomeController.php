7c129c1e9754454e36ce0eaaa73e49b829267ed4
elhaddaji_Otmane <76048466+otmaneelhaddaji@users.noreply.github.com>
2022-06-12 15:56:06 +0100

    creat homeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,12 +6,27 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index($age){
+    public function index(){
         $name="otmane";
+        $post=[[
+            'id'=>1,
+            'title'=>'my first post 1',
+            'body'=>'this is my first post 1'
+        ],
+        [
+            'id'=>2,
+            'title'=>'my first post 2',
+            'body'=>'this is my first post 2'
+        ],
+        [
+            'id'=>3,
+            'title'=>'my first post 3',
+            'body'=>'this is my first post 3'
+        ]];
 
-        return view('/Home')->with(array(
+        return view('home')->with(array(
             'name' => $name,
-            'age' => $age
+            'post' => $post
             // 'age' =>$age
         ));
     }
